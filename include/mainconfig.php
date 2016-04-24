c4e9ec5d279e34cb600b9966c680855d5c3da938
jd5688 <jd5688@gmail.com>
2016-04-24 12:32:39 +1200

    updated config

--- include/mainconfig.php
+++ include/mainconfig.php
@@ -1,20 +1,19 @@
 <?php
-$config['baseurl'] = 'http://www.htmlfivegames.org/sportsbet';
-$config['basedir'] = '/home/htmlfivegames/public_html/sportsbet';
-//$config['baseurl'] = 'http://www.sportsbet.com';
-//$config['basedir'] = '/www/sportsbet';
-$config['private_key'] = 'babyaaric';
+$config['baseurl'] = 'http://YOUR-DOMAIN.COM';
+$config['basedir'] = '/path/to/public/html';
+
+$config['private_key'] = 'your-private-key';
 $config['public_key'] = rand(10000, 50000);
 $config['noreply_email'] = 'noreply@sportsbet.com';
 
 // connect to database
 $DBhost="localhost";
-$DBuser="htmlfive_sports";
-$DBpass="smarttech";
+$DBuser="localhost";
+$DBpass="password";
 
 //$DBuser="dbmanager";
 //$DBpass="smarttech";
 mysql_connect($DBhost,$DBuser,$DBpass);
-mysql_select_db('htmlfive_sportsbet');
+mysql_select_db('database-name');
 mysql_query("SET NAMES 'utf8'");
 ?>
